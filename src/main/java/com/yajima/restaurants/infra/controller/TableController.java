e08c82c53d99bab364529a3419d9f64b73db6ab3
Felipe Yajima Batista <felipe.yajimabatista@gmail.com>
2024-11-17 10:03:53 -0300

    added find tables per restaurant

--- src/main/java/com/yajima/restaurants/infra/controller/TableController.java
+++ src/main/java/com/yajima/restaurants/infra/controller/TableController.java
@@ -2,11 +2,12 @@ package com.yajima.restaurants.infra.controller;
 
 import com.yajima.restaurants.application.usecases.CreateTable;
 import com.yajima.restaurants.application.usecases.ListTables;
-import com.yajima.restaurants.domain.entities.restaurant.Restaurant;
+import com.yajima.restaurants.application.usecases.ListTablesPerRestaurant;
 import com.yajima.restaurants.domain.entities.table.Table;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -16,10 +17,13 @@ public class TableController {
     private final CreateTable createTable;
     private final ListTables listTables;
 
+    private final ListTablesPerRestaurant listTablesPerRestaurant;
 
-    public TableController(CreateTable createTable, ListTables listTables) {
+
+    public TableController(CreateTable createTable, ListTables listTables, ListTablesPerRestaurant listTablesPerRestaurant) {
         this.createTable = createTable;
         this.listTables = listTables;
+        this.listTablesPerRestaurant = listTablesPerRestaurant;
     }
 
     @PostMapping
@@ -40,5 +44,11 @@ public class TableController {
                 )).collect(Collectors.toList());
     }
 
+    @GetMapping("/restaurantId/{id}/")
+    public List<TableDto> ListTablesPerRestaurant(@PathVariable UUID id){
+        return listTablesPerRestaurant.listTablesPerRestaurant(id).stream().map(t -> new TableDto(
+                t.getId(), t.getTableNumber(), t.getNumberOfChairs(), t.getRestaurant()
+        )).collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/yajima/restaurants/infra/persistence/TableRepository.java
+++ src/main/java/com/yajima/restaurants/infra/persistence/TableRepository.java
@@ -1,8 +1,14 @@
 package com.yajima.restaurants.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TableRepository extends JpaRepository<TableEntity, UUID> {
+
+    @Query("select u from TableEntity u where u.restaurantEntity.id = ?1")
+    List<TableEntity> findByRestaurantId(UUID id);
+
 }
